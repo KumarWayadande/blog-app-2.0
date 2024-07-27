10338b2fb43f1ff8180b3a0caccd82d319fd7570
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-07-27 20:54:20 +0530

    Login and Register pages designed

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,29 @@
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <div className="auth">
-        <h1>Login</h1>
-        <form>
-            <input type="text" placeholder="username" name="username" id="username" />
-            <input type="password" placeholder="password" name="password" id="password" />
-            <button>Login</button>
-        </form>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          id="username"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          id="password"
+        />
+        <button>Login</button>
+        <p>This is an error!</p>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
-    <div>
-        Register
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          id="username"
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          id="email"
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          id="password"
+        />
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
     </div>
-  )
+  );
 }
