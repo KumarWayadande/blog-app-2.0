81ea21c5f24371f5253a9b6313aadb7df9ceb86e
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-07-28 12:21:28 +0530

    react-quill package installed

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 export default function Write() {
   return (
-    <div>
-      Write
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" name="title" id="title" />
+      </div>
+      <div className="menu">
+        <div className="item">i1</div>
+        <div className="item">i2</div>
+      </div>
     </div>
-  )
+  );
 }
